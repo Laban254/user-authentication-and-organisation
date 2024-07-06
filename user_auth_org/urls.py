ec0415e216549c88c35d728177238d5841bab59b
laban254 <labanrotich6544@gmail.com>
2024-07-07 02:20:26 +0300

    welcome page

--- user_auth_org/urls.py
+++ user_auth_org/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView
+from accounts.views import welcome_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+     path('', welcome_view, name='welcome'),
     
 ]
